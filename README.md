f6c4d987449783d48b6a09eb7f056747ed22f4f7
Alex <alexandr.movergan@altenar.com>
2023-05-09 12:13:43 +0200

    Added migrations and updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Simple todo web app made for learning purpose
 
+1. Prepare local development setup. This application needs MySQL DB in order to work. Connection details are configured in `conn.php` file. You can use Docker.
+You may want to use one of the following options. They all good but please check them before making the decision on what to use:
+- https://hub.docker.com/_/mysql  
+- https://hub.docker.com/_/mariadb
+- https://hub.docker.com/r/bitnami/mysql
+- https://hub.docker.com/r/bitnami/mariadb
+
+Make sure you can connect to the database using user and password from the connection string file. You can find preparation scripts in the `migrations` folder. 
+
+2. Once you are able to connect to a database using CLI and username and password from the `conn.php` file, please make sure DB structure is created as well. 
+3. DB part is done, get yourself familiar with the code and let's start preparing Nginx+PHP-FPM for running the website. We will use the following instructions: 
+- https://hub.docker.com/r/bitnami/php-fpm (The "Serving your PHP app through an nginx frontend" part)
---
+++ migrations/01_create_db_structure.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.14  Distrib 5.5.68-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: id2061448_mysql
+-- ------------------------------------------------------
+-- Server version	5.5.68-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dd`
+--
+
+DROP TABLE IF EXISTS `dd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dd` (
+  `user` varchar(50) NOT NULL,
+  `task` varchar(50) NOT NULL,
+  `st` time NOT NULL,
+  `tme` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dd`
+--
+
+LOCK TABLES `dd` WRITE;
+/*!40000 ALTER TABLE `dd` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dd` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reg`
+--
+
+DROP TABLE IF EXISTS `reg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reg` (
+  `user` varchar(128) NOT NULL,
+  `pass` varchar(128) NOT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reg`
+--
+
+LOCK TABLES `reg` WRITE;
+/*!40000 ALTER TABLE `reg` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reg` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `user` varchar(11) NOT NULL,
+  `task` varchar(50) NOT NULL,
+  `st` time(6) NOT NULL,
+  `tme` time(6) NOT NULL,
+  `pre` varchar(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'id2061448_mysql'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-09 16:28:16
